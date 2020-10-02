3333043fe2ecffa3d357ff8d8e85cb7e7382bd33
Hari58 <71202765+Hari58@users.noreply.github.com>
2020-10-02 13:35:03 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,25 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() 
+	{
+		
+		System.out.println("College Name: RKM Vivekananda College");
+
+	}
+	
+	public void collegeCode()
+	{
+		
+		System.out.println("College code: 123");
+
+	}
+	
+	public void collegeRank() 
+	{
+	
+		System.out.println("College Rank: 7");
+	
+	}
+}
---
+++ student/Student.java
@@ -0,0 +1,39 @@
+package org.student;
+
+import org.department.Department;
+
+public class Student extends Department
+{
+
+	public void studentName() {
+		
+		System.out.println("Student Name: Hari");
+
+	}
+	
+	public void studentDept() {
+		
+		System.out.println("Student Department: B.Sc");
+
+	}
+	
+	public void studentId() {
+		
+		System.out.println("Student ID: UCS1208");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Student st = new Student();
+		st.collegeName();
+		st.collegeCode();
+		st.collegeRank();
+		st.deptName();
+		st.studentName();
+		st.studentDept();
+		st.studentId();
+
+	}
+
+}
